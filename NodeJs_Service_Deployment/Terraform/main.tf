8ce33d072f5b4698df09f0874ee059cdcf6ec595
hassenkhalifa <khalifa.hassen.pro@gmail.com>
2025-06-17 15:37:52 +0200

    modif terraform

--- NodeJs_Service_Deployment/Terraform/main.tf
+++ NodeJs_Service_Deployment/Terraform/main.tf
@@ -11,6 +11,8 @@ terraform {
 resource "aws_key_pair" "deploy" {
   key_name   = "deploy-key"
   public_key = var.ssh_public_key
+
+
 }
 
 resource "aws_security_group" "ssh_access" {
@@ -35,6 +37,7 @@ resource "aws_security_group" "ssh_access" {
   tags = {
     Name = "allow_ssh"
   }
+
 }
 
 resource "aws_security_group" "Node_App" {
@@ -59,16 +62,36 @@ resource "aws_security_group" "Node_App" {
   tags = {
     Name = "Node_App"
   }
+
 }
 
 resource "aws_instance" "ubuntu_server" {
   ami                         = "ami-020cba7c55df1f615"
   instance_type               = "t2.micro"
-  key_name                    = aws_key_pair.deploy.key_name                                               
-  vpc_security_group_ids      = [aws_security_group.ssh_access.id, aws_security_group.Node_App.id] 
+  key_name                    = aws_key_pair.deploy.key_name
+  vpc_security_group_ids      = [aws_security_group.ssh_access.id, aws_security_group.Node_App.id]
   associate_public_ip_address = true
 
   tags = {
     Name = "Ubuntu-Server"
   }
+
+}
+
+data "aws_route53_zone" "selected" {
+  name         = "hassendevops.com."
+  private_zone = false
+}
+
+resource "aws_eip" "static_ip" {
+  instance = aws_instance.ubuntu_server.id
+  domain   = "vpc"
+}
+
+resource "aws_route53_record" "dns_record" {
+  zone_id = data.aws_route53_zone.selected.zone_id
+  name    = "ec2.hassendevops.com"
+  type    = "A"
+  ttl     = 300
+  records = [aws_eip.static_ip.public_ip]
 }
